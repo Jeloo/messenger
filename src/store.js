05a4348c1caf6f4cef5fc4b61469a6e79bf5ea9e
Jeloo <i.v.yaroslavtsev@gmail.com>
2018-05-11 00:12:19 +0300

    Fix redux-logger middleware initialization

--- src/store.js
+++ src/store.js
@@ -10,7 +10,7 @@ export const history = createHistory();
 
 const createMiddleware = () => {
     const common = [thunk, routerMiddleware(history)],
-          dev    = [createLogger];
+          dev    = [createLogger()];
 
     if (process.env.NODE_ENV === 'production') {
         return applyMiddleware(...common);
